db3c158ed03c07677c5685023d8ea2b4145e00e2
FREDYFREDBOB <fredyfredbob@gmail.com>
2019-11-18 14:23:30 -0800

    started implementing the onclicks

--- app/src/main/java/com/example/findmyband/discovery.java
+++ app/src/main/java/com/example/findmyband/discovery.java
@@ -1,13 +1,17 @@
 package com.example.findmyband;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class discovery extends AppCompatActivity {
+public class discovery extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {//implementation
 
     private DrawerLayout drawer;
 
@@ -20,12 +24,28 @@ public class discovery extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        //*****
+        NavigationView navigationView = findViewById(R.id.nav_view);//listener for navigation method
+        navigationView.setNavigationItemSelectedListener(this);
+        //*****
+
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
     }
-
+//navigation method
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        switch (item.getItemId()){
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new);
+                break;
+        }
+        return true;
+    }
+//*************
     @Override
     public void onBackPressed() {
         if(drawer.isDrawerOpen(GravityCompat.START)){
